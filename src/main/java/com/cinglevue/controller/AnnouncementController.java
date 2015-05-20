91803e494c0c0eae6e3409a72f600ab41cd20790
Chathura de Silve <chathurait@gmail.com>
2015-05-20 20:18:44 +0530

    #adding log messages

--- src/main/java/com/cinglevue/controller/AnnouncementController.java
+++ src/main/java/com/cinglevue/controller/AnnouncementController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.websocket.server.PathParam;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,30 +23,37 @@ public class AnnouncementController {
 	@Autowired
 	private AnnouncementService announcementService;
 	
+	private Logger logger = LoggerFactory.getLogger(AnnouncementController.class);
+		
 	@RequestMapping(value = "")
     public List<Announcement> findAllAnnouncements() {
+		logger.info("findAllAnnouncements() API");
         return announcementService.getAllEntities();
     }
 	
 	@RequestMapping(value = "/{announcementId}")
 	public Announcement findOne(@PathVariable(value="announcementId") String announcementId){
 		//return null;
+		logger.info("findOne API");
 		return announcementService.getEntity(announcementId);
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
 	public Announcement addOne(@RequestBody Announcement announcement){
 		//return null;
+		logger.info("addOne API");
 		return announcementService.insertEntity(announcement);
 	}
 	
 	@RequestMapping(method=RequestMethod.PUT, value = "/{announcementId}")
 	public Announcement updateOne(@RequestBody Announcement announcement){
+		logger.info("updateOne API");
 		return announcementService.updateEntity(announcement);
 	}
 	
 	@RequestMapping(method=RequestMethod.DELETE, value = "/{announcementId}")
 	public Announcement deleteOne(@RequestBody Announcement announcement){
+		logger.info("deleteOne API");
 		return announcementService.deleteEntity(announcement);
 	}
 }
--- src/main/java/com/cinglevue/service/impl/AnnouncementServiceImpl.java
+++ src/main/java/com/cinglevue/service/impl/AnnouncementServiceImpl.java
@@ -2,10 +2,13 @@ package com.cinglevue.service.impl;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.cinglevue.controller.AnnouncementController;
 import com.cinglevue.domain.Announcement;
 import com.cinglevue.repository.AnnouncementRepository;
 import com.cinglevue.service.AnnouncementService;
@@ -15,26 +18,32 @@ public class AnnouncementServiceImpl implements AnnouncementService{
 	
 	@Autowired
 	private AnnouncementRepository repository;
+	private Logger logger = LoggerFactory.getLogger(AnnouncementServiceImpl.class);
 
 	@Override
 	public List<Announcement> getAllEntities() {
 		// TODO Auto-generated method stub
+		logger.info("getAllEntities API");
 		return repository.findAll();
 	}
 	
 	public Announcement getEntity(String id){
+		logger.info("getEntity API");
 		return repository.findOne(id);
 	}
 	
 	public Announcement insertEntity(@RequestBody Announcement announcement){
+		logger.info("insertEntity API");
 		return repository.save(announcement);		
 	}
 	
 	public Announcement updateEntity(Announcement announcement){
+		logger.info("updateEntityAPI");
 		return repository.save(announcement);
 		
 	}	
 	public Announcement deleteEntity(Announcement announcement){
+		logger.info("deleteEntity API");
 		announcement.setActive((Boolean.FALSE));
 		return repository.save(announcement);
 	}
